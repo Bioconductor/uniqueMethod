883b7c1623ee15d4e5ae7ef0211e0f9404219f93
Hervé Pagès <hpages@fredhutch.org>
2018-04-29 17:20:46 -0700

    small tweak

--- R/unique-method.R
+++ R/unique-method.R
@@ -3,8 +3,7 @@ setClass("A", slots=c(stuff="ANY"))
 setMethod("unique", "A",
     function(x, incomparables=FALSE, ...)
     {
-        x@stuff <- unique(x@stuff, incomparables=incomparables, ...)
-        x
+        cat("Hi, I'm the uniqueMethod::unique() method for A objects.\n")
     }
 )
 
